f8cf5f85dff9c97084d695d027a9c442ea578844
Charles Reusnow <Charles.J.Reusnow@centene.com>
2017-10-28 21:14:04 -0500

    swag

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,72 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { Validators,FormControl,FormGroup,FormBuilder } from '@angular/forms';
-import {Router} from "@angular/router";
-import {AuthService} from "../services/auth.service";
-import {HttpService} from "../services/http.service";
-import { User } from '../model/user';
+import {Component, OnInit} from "@angular/core";
+import {Headers, Http, RequestOptions, Response} from "@angular/http";
 
 @Component({
-  selector: 'arch-hacks-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  selector: "arch-hacks-registration",
+  templateUrl: "./registration.component.html",
+  styleUrls: ["./registration.component.scss"]
 })
 export class RegistrationComponent implements OnInit {
-  public userForm: FormGroup;
-  public emailRegex: string = "[^ @]*@[^ @]*"
-  public user: User = {
-    email: '',
-    username: '',
-    password: '',
-    basalRate: 0,
-    carbRatio: 0,
-    correctionRatio: 0,
-    IOBDuration: 0
+  private static readonly BACK_END_BASE_URL: string = "http://localhost:3000";
+
+  username: string = "";
+  password: string = "";
+  confirmPassword: string = "";
+  email: string = "";
+  basalRate: string = "";
+  carbohydrateRatio: string = "";
+  correctionRatio: string = "";
+  targetBloodSugar: string = "";
+  insulinOnBoarDuration: string = "";
+
+  constructor(private http: Http) {
   }
 
-  constructor(public fB: FormBuilder,
-              public authService: AuthService,
-              public router: Router,
-              public httpService: HttpService) {  
+  ngOnInit(): void {
   }
 
-  ngOnInit() {
-      this.userForm = this.fB.group({
-          'username': new FormControl('', Validators.compose([
-              Validators.required,
-              Validators.minLength(6)
-          ])),
-          'email': new FormControl('', Validators.compose([
-              Validators.required,
-              Validators.pattern(this.emailRegex)
-          ])),
-          'password': new FormControl('', Validators.compose([
-              Validators.required,
-              Validators.minLength(6)
-          ])),
-          'basalRate': new FormControl(''),
-          'carbRatio': new FormControl(''),
-          'correctionRatio': new FormControl({}),
-          'IOBDuration': new FormControl({})
-      });
+  register() {
+    if (this.validCredentials()) {
+      const BODY: any = {
+        "username": this.username,
+        "password": this.password,
+        "email": this.email,
+        "basalRate": this.basalRate,
+        "carbohydrateRatio": this.carbohydrateRatio,
+        "correctionRatio": this.correctionRatio,
+        "targetBloodSugar": this.targetBloodSugar,
+        "insulinOnBoardDuration": this.insulinOnBoarDuration
+      };
+
+      const REQUEST_OPTIONS: RequestOptions = new RequestOptions(
+        {
+          "headers": new Headers(
+            {
+              "Content-Type": "application/json"
+            }
+          )
+        }
+      );
+
+      this.http.post(RegistrationComponent.BACK_END_BASE_URL + "/users/register", BODY, REQUEST_OPTIONS).subscribe(
+        (response: Response): void => {
+          const RESPONSE_BODY: string = response.json();
+
+          console.log(RESPONSE_BODY);
+        },
+        (error: any): void => {
+          console.error(error);
+        }
+      );
+    }
   }
 
-  onRegisterSubmit(value){
-    this.user = value;
-    this.authService.registerUser(this.user)
-            .subscribe(
-                (data: any): void => {
-                    if (data.success) {
-                        let body: any = {
-                            email: this.user.email,
-                            username: this.user.username,
-                            password: this.user.password,
-                            basalRate: this.user.basalRate,
-                            carbRatio: this.user.carbRatio,
-                            correctionRatio: this.user.correctionRatio,
-                            IOBDuration: this.user.IOBDuration
-                        };
-                    } 
-                },
-            );
+  validCredentials(): boolean {
+    return true; // TODO (lol)
   }
 }
